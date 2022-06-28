1c6b3431c246d569913abcc92a7580c362503293
Henrique <94632721+OsvaldoHenriqueSouza@users.noreply.github.com>
2022-06-28 19:51:36 -0300

    Requisito 4, inicio

---
+++ src/tests/Login.test.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import App from '../App'
+import { screen, waitFor } from '@testing-library/react';
+import renderWithRouterAndRedux from './helpers/renderWithRouterAndRedux';
+import userEvent from '@testing-library/user-event';
+
+const data = {
+  "response_code":0,
+  "response_message":"Token Generated Successfully!",
+  "token":"f00cb469ce38726ee00a7c6836761b0a4fb808181a125dcde6d50a9f3c9127b6"
+};
+
+const mockFetch = global.fetch = jest.fn(async () => ({
+  json: async () => data
+}));
+
+describe('Teste da página Login', () => {
+  test('Verifica se os inputs estão renderizados', () => {
+    renderWithRouterAndRedux(<App />);
+    const inputName = screen.getByTestId('input-player-name');
+    const inputEmail = screen.getByTestId('input-gravatar-email');
+    const buttonPlay = screen.getByTestId('btn-play')
+    const buttonSettings = screen.getByTestId('btn-settings')
+    expect(inputName).toBeInTheDocument();
+    expect(inputEmail).toBeInTheDocument();
+    expect(buttonPlay).toBeInTheDocument();
+    expect(buttonSettings).toBeInTheDocument();
+    expect(buttonPlay).toBeDisabled();
+    userEvent.type(inputName, 'Lucas');
+    userEvent.type(inputEmail, 'lucaslindao@hotmail.com');
+    expect(buttonPlay).not.toBeDisabled();
+  });
+  // test('Verifica o butão Play', async () => {
+  //   renderWithRouterAndRedux(<App />)
+  //   const buttonPlay = await screen.findByTestId('btn-play')
+  //   userEvent.click(buttonPlay);
+  //   expect(global.fetch).toHaveBeenCalled();
+  // });
+  test(`Verifica se o butão "configuração"
+  redireciona para a página correta`, () => {
+    const { history } = renderWithRouterAndRedux(<App />);
+    const buttonSettings = screen.getByTestId('btn-settings');
+    userEvent.click(buttonSettings);
+    const { pathname } = history.location;
+    expect(pathname).toEqual("/settings");
+    
+  });
+  test('', () => {
+    renderWithRouterAndRedux(<App />);
+    const buttonSettings = screen.getByTestId('btn-settings');
+    userEvent.click(buttonSettings);
+    const divSeting = screen.getByTestId('settings-title');
+    expect(divSeting).toBeInTheDocument();
+  });
+})
