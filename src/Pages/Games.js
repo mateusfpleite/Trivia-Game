0a8162062c566e4787d340837fb7595dc705405a
Mateus <mateusfpleite@gmail.com>
2022-06-29 15:43:32 -0300

    requisito 5

---
+++ src/Pages/Games.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../Components/Header';
+
+class Games extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+      </div>
+    );
+  }
+}
+
+export default Games;
